758a8bd5587fee658e78d985ead3dc091c95ddb5
nguyenphamannsg <68599676+nguyenphamannsg@users.noreply.github.com>
2021-04-07 04:24:08 +0700

    Update jvectormap-setting.js

--- vendors/scripts/jvectormap-setting.js
+++ vendors/scripts/jvectormap-setting.js
@@ -1,15 +1,1 @@
-jQuery('#chart1').vectorMap({
-	map: 'world_mill_en',
-	backgroundColor: '#1b00ff',
-	borderWidth: 1,
-	zoomOnScroll : false,
-	color: '#ddd',
-	regionStyle: {
-		initial: {
-			fill: '#fff'
-		}
-	},
-	enableZoom: true,
-	normalizeFunction: 'linear',
-	showTooltip: true
-});
+jQuery("#chart1").vectorMap({map:"world_mill_en",backgroundColor:"#1b00ff",borderWidth:1,zoomOnScroll:!1,color:"#ddd",regionStyle:{initial:{fill:"#fff"}},enableZoom:!0,normalizeFunction:"linear",showTooltip:!0});
